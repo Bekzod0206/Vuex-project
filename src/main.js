700608c49a5627ef6fc2eb1c60d2c0dd0e76de04
Bekzod <davronovbekzod20@gmail.com>
2023-02-13 23:48:26 +0500

    vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import uiComponens from './ui-componens'
 
 const app = createApp(App)
 
 uiComponens.map(item => app.component(item.name, item))
 app.use(router)
+app.use(store)
 app.mount('#app')
