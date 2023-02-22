14a1b84969a189b5e2d356055b92474839169a5a
Bekzod <davronovbekzod20@gmail.com>
2023-02-22 23:37:19 +0500

    #41 VueX getters

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
         <template v-if="isLoggedIn"> 
           <Router-link :to="{name: 'home'}" class="me-3 py-2 text-dark text-decoration-none">{{ currentUser.username }}</Router-link>
         </template>
-        <template v-if="!isLoggedIn">
+        <template v-if="isAnonymous">
           <Router-link :to="{name: 'login'}" class="me-3 py-2 text-dark text-decoration-none">Login</Router-link>
           
           <Router-link :to="{name: 'register'}" class="me-3 py-2 text-dark text-decoration-none">Register</Router-link>
@@ -18,8 +18,9 @@
 </template>
 
 <script>
-import {mapState} from "vuex"
+import {mapGetters} from "vuex"
 import {logo} from '../constants'
+import {gettersTypes} from '@/modules/types'
 export default {
   data(){
     return{
@@ -27,10 +28,11 @@ export default {
     }
   },
   computed: {
-    ...mapState({
-      currentUser: state => state.auth.user,
-      isLoggedIn: state => state.auth.isLoggedIn,
-    })
+    ...mapGetters({
+      currentUser: gettersTypes.currentUser,
+      isLoggedIn: gettersTypes.isLoggedIn,
+      isAnonymous: gettersTypes.isAnonymous,
+    }),
   },
   methods: {
     toHomeHandler(){
